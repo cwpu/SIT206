c8692fe12e618fbceb0fe42ed6311ee9ddf1b4f9
cwpu <xhll410117@gmail.com>
2018-05-12 16:28:21 +0800

    Creat BaseClass

---
+++ Stu-SIT206/Stu-SIT206/Base/BaseTableViewController.swift
@@ -0,0 +1,35 @@
+//
+//  BaseTableViewController.swift
+//  StudentSIT206
+//
+//  Created by cpwu on 2018/5/12.
+//  Copyright © 2018年 cpwu. All rights reserved.
+//
+
+import UIKit
+
+class BaseTableViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Stu-SIT206/Stu-SIT206/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Stu-SIT206
+//
+//  Created by cpwu on 2018/5/12.
+//  Copyright © 2018年 cpwu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
